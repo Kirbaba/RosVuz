00d5c7c37c942a9c796ca24f3246bb0fee90378a
Vladislav <v.kirbaba@gmail.com>
2015-09-11 13:49:22 +0300

    Выпадающая карта

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/*
+ * Created with Sublime Text 2.
+ * User: song.chen
+ * Date: 2015-09-11
+ * Time: 13:37:45
+ * Contact: song.chen@qunar.com
+ */
+/*
+ * Created with Sublime Text 2.
+ * User: song.chen
+ * Date: 2015-09-11
+ * Time: 12:52:47
+ * Contact: song.chen@qunar.com
+ */
+
+
 $('#inputDate').DatePicker({ 
   date: $('#inputDate').val(),
   current: $('#inputDate').val(),
@@ -13,8 +29,17 @@ $('#inputDate').DatePicker({
   }
 });
 
-
 $(document).ready(function(){
+
+    $('.audience').click(function() {
+      $(".showMap").slideToggle('slow', function(){
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initialize";
+        document.body.appendChild(script);  
+      });         
+    });
+
     $('input.timepicker').timepicker({
         timeFormat: 'HH:mm',
         // year, month, day and seconds are not important
@@ -32,5 +57,40 @@ $(document).ready(function(){
     });
 });
 
+/*-------------GOOGLE MAPS-----------------*/
 
+function initialize() {
 
+    var myLatlng = new google.maps.LatLng(55.662561,37.540873);
+    var mapOptions = {
+        center: new google.maps.LatLng(55.662561,37.540873),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false
+    };
+    var contentString = '<div id="siteNotice">'+ '<p>г. Москва, ул. Нахимсона д. 65,</br> строение №3, аудитория №103а</p>'
+      +'</div>';
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);    
+    var image = '../img/map_icon.png';
+    var myLatLng = new google.maps.LatLng(55.662561,37.540873);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
+    beachMarker.addListener('click', function() {
+    infowindow.open(map, beachMarker);
+  });
+}
+
+// function loadScript() {
+//     var script = document.createElement("script");
+//     script.type = "text/javascript";
+//     script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE&callback=initialize";
+//     document.body.appendChild(script);
+// }
+
+window.onload = loadScript;
